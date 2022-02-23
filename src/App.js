4efb5b11fc7bd20c01c2a76cd9604a08b43a36df
unknown <ghostshabet@gmail.com>
2022-02-23 11:14:03 +0100

    ossature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={process.env.PUBLIC_URL + "/logo-clean3000-transparent.jpg"}
+          alt="cleanheader"
+        />
+        ;
       </header>
+
+      <div className="form-group">
+        <label htmlFor="inputCompagny">Nom entreprise ou client :</label>
+        <input id="inputCompagny" type="text" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="inputWorker">
+          Notre technicien est intervenu pour l'entretien de la vitrerie le :
+        </label>
+        <input id="inputWorker" type="text" />
+      </div>
+      <div className="form-group">
+        <textarea>Observation</textarea>
+      </div>
+      <div className="merci">Merci de votre confiance</div>
     </div>
   );
 }
-
 export default App;
